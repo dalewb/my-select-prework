9eb6259b3d6b32b8eb51710a592109dbd6b2af47
William Dale <williambdale@gmail.com>
2018-03-07 17:45:06 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,4 +1,14 @@
 def my_select(collection)
   i = 0 
-  
+  result = [] 
+  while i < collection.length 
+    a = yield array[i] 
+    if a 
+      result << a 
+    end 
+    i += 1
+  end 
+  result 
 end
+
+my_select([1,2,3,4,5]) {|el| el.odd?}
